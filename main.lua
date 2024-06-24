8be227f843fbefa7ab0388366bce3bac7cb716a9
decamel <den_spb97@mail.ru>
2024-06-24 20:03:06 +0300

    lua example extended

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
 local logger = Logger.new("lua")
+local publish = Mqtt.new("events/vessel/heading")
+
+local parser = Nmea.new()
 
 local op = Operator.new("gnss")
-
-op.source = Traffic.tcp()
 
 op.routes = { "DSS/1/1", "DSS/1/2" }
 
 op.next = function(message)
+  local sentence = parse(message)
+
+  local heading = sentence:field(0)
+
+  local payload = Response.new("heading")
+  payload.value = heading
+
   logger:info("Hello from Lua: " .. message)
+  publish(payload)
 end
 
 
